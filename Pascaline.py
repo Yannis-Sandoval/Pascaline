4eb5f94ce8a459ab9ed28ee8ddefd302031aa98d
Yannisand <yannis.sandoval22@gmail.com>
2026-01-12 15:40:58 +0100

    Defining main defs

--- Pascaline.py
+++ Pascaline.py
@@ -1,1 +1,16 @@
-print("test")
+
+
+#Vérifie si le caractère est valide entre chiffres, symboles et parenthèses
+def testvalidinput():
+
+#Vérifie le type de caractère entre chiffres, symboles et parenthèses
+def testinputtype():
+
+#Vérifie la priorité actuelle
+def testpriority():
+
+#Effectue le calcul à la plus haute priorité
+def current_calculation():
+
+#Branche principale de la calculette
+def calculator():
