e77621df20ed0850c0a8d4a36287a86ed8811a31
willLaPlateforme <guillaume.ciampa@laplateforme.io>
2026-01-12 16:01:18 +0100

    Update defs

--- Pascaline.py
+++ Pascaline.py
@@ -1,16 +1,31 @@
-
-
 #Vérifie si le caractère est valide entre chiffres, symboles et parenthèses
 def testvalidinput():
+    
+    print("Erreure")
+
+def test_valid_calculation():
+
+    print("Chiffre non valide")
 
 #Vérifie le type de caractère entre chiffres, symboles et parenthèses
 def testinputtype():
 
+
+
 #Vérifie la priorité actuelle
 def testpriority():
+
+
 
 #Effectue le calcul à la plus haute priorité
 def current_calculation():
 
+    
+
 #Branche principale de la calculette
-def calculator():
+def calculator(calcule):
+    
+    
+
+
+calculator(input("Entrer le calcule"))
