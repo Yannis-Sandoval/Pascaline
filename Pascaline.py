3e6c8ef87e3db10b562a78db3347ec658c2db033
Yannisand <yannis.sandoval22@gmail.com>
2026-01-13 11:45:10 +0100

    Start defs testvalidinput, testvalidcalculation, testinputtype, currentcalculation

--- Pascaline.py
+++ Pascaline.py
@@ -1,15 +1,45 @@
+Symbols = ["+","-","*","/","(",")"]
+
+
 #Vérifie si le caractère est valide entre chiffres, symboles et parenthèses
-def testvalidinput():
-    
-    print("Erreure")
+def testvalidinput(Character):
+    Validinput = False
+    if Character in Symbols:
+        Validinput = "Symbol"
+    else:
+        try:
+            Character = int(Character)
+            Validinput = "Integer"
+        except:
+            print("Erreur")
+            print("Veuillez entrer des caractères valides")
+    return Validinput
 
-def test_valid_calculation():
+# Vérifie si les calculs sont authorités
+def testvalidcalculation(Character,NextCharacter):
+    if Character == "/":
+        if NextCharacter == "0":
+            print("Erreur")
+            print("Chiffre non valide")
+            return False
+    else:
+        return True
 
-    print("Chiffre non valide")
 
 #Vérifie le type de caractère entre chiffres, symboles et parenthèses
-def testinputtype():
-
+def testinputtype(Calculation):
+    Currentnumber = ""
+    Intlist = ""
+    Symblist = ""
+    for i in range(len(Calculation)):
+        Inputtype = testvalidinput(Calculation[i])
+        if Inputtype == "Integer":
+            Currentnumber = Currentnumber + Inputtype
+        elif Inputtype == "Symbol":
+            Symblist = Symblist + [Inputtype]
+            if testvalidinput(Calculation[i-1]) == "Integer":
+                Intlist = Intlist + [int(Currentnumber)]
+                Currentnumber = ""
 
 
 #Vérifie la priorité actuelle
@@ -18,8 +48,17 @@ def testpriority():
 
 
 #Effectue le calcul à la plus haute priorité
-def current_calculation():
+def currentcalculation(Value1,Symbols,Value2):
 
+    if Symbols == "+":
+        Result = Value1 + Value2
+    elif Symbols == "-":
+        Result = Value1 - Value2
+    elif Symbols == "*":
+        Result = Value1 * Value2
+    elif Symbols == "/":
+        Result = Value1 / Value2
+    return Result
     
 
 #Branche principale de la calculette
@@ -27,5 +66,4 @@ def calculator(calcule):
     
     
 
-
-calculator(input("Entrer le calcule"))
+calculator(input("Entrer le calcul"))
